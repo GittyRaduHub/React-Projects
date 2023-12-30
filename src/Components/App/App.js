1421104f37ce9627bbc77dda88256ff068bf313b
Radu <131166023+GittyRaduHub@users.noreply.github.com>
2023-12-30 13:12:27 +0200

    Add files via upload

---
+++ src/Components/App/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const iubitBtnClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      {count === 0 ? (
+        <h1>Ma iubesti?</h1>
+      ) : (
+        <h1> M-ai iubit de {count} ori!</h1>
+      )}
+
+      {count === 0 ? (
+        <img className="imgSad" src="/poze/sadCat.jpg" alt="sadCat"></img>
+      ) : (
+        <img className="imgHappy" src="/poze/kitty.gif" alt="happyCat"></img>
+      )}
+      <button onClick={iubitBtnClick}>Te iubesc!</button>
+    </div>
+  );
+}
+
+export default App;
